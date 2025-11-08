5de3a7c29a03b4aae3f70d39f508d6f28139aa48
hurix <simon@hurix.de>
2025-11-08 22:11:26 +0100

    switch parsing from html to api

--- scripts/smwcparser.js
+++ scripts/smwcparser.js
@@ -6,19 +6,21 @@
 
 'use strict';
 
-const
-    https = require('https'),
-    fs = require('fs'),
-
-    nodeHtmlParser = require('node-html-parser')
-;
+const https = require('https');
 
 async function getRomhackInfo_Async(romhackName)
 {
     try {
         let romhackName_Parsed = getParsedRomhackName(romhackName);
-        let html = await getHtml_Async(romhackName);
-        let info = getRomhackInfo_FromHtml(html, romhackName_Parsed);
+        let jsondata = await getHtml_Async(romhackName);
+        let info = getRomhackInfo_FromJson(jsondata, romhackName_Parsed);
+
+        const error = info.error || {};
+        if (error.type === 'no-results')
+        {
+            jsondata = await getHtml_Async(romhackName, '&u=1');
+            info = getRomhackInfo_FromJson(jsondata, romhackName_Parsed);
+        }
 
         return info;
     } catch (e) {
@@ -30,26 +32,26 @@ async function getRomhackInfo_Async(romhackName)
         };
     }
 }
-exports.getRomhackInfo_Async = getRomhackInfo_Async;
+module.exports = getRomhackInfo_Async;
 
 
-function getHtml_Async(romhackName)
+function getHtml_Async(romhackName, waiting='')
 {
     return new Promise((resolve, reject) => {
         let body = '';
         var r = https.request({
             host: 'www.smwcentral.net',
             port: '443',
-            path: encodeURI('/?p=section&s=smwhacks&f[name]=' + romhackName),
+            path: encodeURI('/ajax.php?a=getsectionlist&s=smwhacks&f[name]=' + romhackName + waiting),
             method: 'GET',
             headers: {},
-                }, (res) => {
+        }, (res) => {
             res.setEncoding('utf8');
             res.on('data', (chunk) => {
                 body += chunk;
             });
             res.on('end', () => {
-                resolve(body);
+                resolve(JSON.parse(body));
             })
         });
         r.on('error', (err) => {
@@ -64,12 +66,13 @@ function getParsedRomhackName(romhackName_Raw)
 {
     romhackName_Raw = romhackName_Raw.toLowerCase();
 
-    let letters_Allowed = '1234567890' + 
-            'qwertyuiop' + 
-            'asdfghjkl' +
-            'zxcvbnm' +
-            ' ';
-        
+    let letters_Allowed = '1234567890' +
+        'qwertyuiop' +
+        'asdfghjkl' +
+        'zxcvbnm' +
+		'\'' +
+        ' ';
+
     let romhackName_Parsed = '';
     for (let i = 0; i < romhackName_Raw.length; i++) {
         if (letters_Allowed.indexOf(romhackName_Raw[i]) === -1)
@@ -82,50 +85,40 @@ function getParsedRomhackName(romhackName_Raw)
 }
 
 
-function getRomhackInfo_FromHtml(html, romhackName_Parsed)
+function getRomhackInfo_FromHackJson(hack)
 {
-    let root = nodeHtmlParser.parse(html);
-
-    let table = root.querySelector('.list');
-    let tbody = table.querySelector('tbody');
-    let trs = tbody.querySelectorAll('tr');
-    if (trs.length === 0) {
-        return {
-            error: {
-                type: 'no-results',
-            },
-        };
-    }
-
-    if (trs.length === 1)
-        return getRomhackInfo_FromTr(trs[0]);
-
-    for (let tr of trs) {
-        let romhackInfo = getRomhackInfo_FromTr(tr);
-        if (getParsedRomhackName(romhackInfo.name) === romhackName_Parsed)
-            return romhackInfo;
-    }
-
-    return {
-        error: {
-            type: 'multiple-results',
-        },
-    };
+  let authors = hack.authors.map(author => author.name);
+  return {
+    error: null,
+    url:  `https://www.smwcentral.net/?p=section&a=details&id=${hack.id}`,
+    name: hack.name,
+    exits: hack.raw_fields.length,
+    type: hack.fields.type,
+    author: hack.authors[0].name,
+    authors
+  };
 }
 
-function getRomhackInfo_FromTr(tr)
-{
-    let tds = tr.querySelectorAll('td');
-    
-	let exit = /[0-9]*/g;
-	let exitN = exit.exec(tds[3].innerHTML);
-	
-    return {
-        error: null,
 
-        name: tds[0].querySelector('a').innerHTML,
-        exits: exitN[0],
-        type: tds[4].innerHTML,
-        author: tds[5].querySelector('a').innerHTML,
-    };
+function getRomhackInfo_FromJson(jsondata, romhackName_Parsed)
+{
+  if (jsondata.total === 0 || !jsondata.data)
+    return {error: {type: 'no-results', data: jsondata},};
+
+  let hacks = jsondata.data;
+
+  if (hacks.length === 1)
+      return getRomhackInfo_FromHackJson(hacks[0]);
+
+  for (let hack of hacks) {
+      let romhackInfo = getRomhackInfo_FromHackJson(hack);
+      if (getParsedRomhackName(romhackInfo.name) === romhackName_Parsed)
+          return romhackInfo;
+  }
+
+  return {
+      error: {
+          type: 'multiple-results',
+      },
+  };
 }
